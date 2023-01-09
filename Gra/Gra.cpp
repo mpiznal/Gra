695f4864461f2c7773da8d77b05f546129a9a303
mpiznal <29964@s.pm.szczecin.pl>
2023-01-10 00:13:26 +0100

    zwiekszenie czytelnosci gry (komunikaty dla gracza), proba wypisania prawidlowo oznaczonej planszy i pol na ekran

--- Gra/Gra.cpp
+++ Gra/Gra.cpp
@@ -2,43 +2,76 @@
 using namespace std;
 
 
+
 void rysuj_plansze() {
+	int wiersze=0, kolumny=0;
 	int** plansza, w, k;
-	cout << "Podaj liczbe wierszy i kolumn (najmniejsza powierzchnia planszy to 8x8). " << endl;
-	cin >> w >> k;
-	if (w < 8 || k < 8) {
+	cout << "Aby narysowac plansze: podaj liczbe wierszy i kolumn (najmniejsza wymiary planszy to 8x8). " << endl;
+	cout << "Liczba wierszy: " << endl;
+	cin >> wiersze;
+	cout << "Liczba kolumn: " << endl;
+	cin >> kolumny;
+	if (wiersze < 8 || kolumny < 8) {
 		cout << "Wpisales za malo wierszy i(lub) kolumn, wpisz jeszcze raz: " << endl;
 		rysuj_plansze();
 	}
 	else {
 		cout << "Wpisales prawidlowe liczby wierszy i kolumn, wymiary Twojej planszy to: " << endl;
-		cout << "Wiersze: " << w << endl;
-		cout << "Kolumny: " << k << endl;
+		cout << "Wiersze: " << wiersze << endl;
+		cout << "Kolumny: " << kolumny << endl;
 	}
+	w = wiersze + 1;
+	k = kolumny + 1;
+
 	plansza = new int* [w];
 
 	for (int i = 0; i < w; i++)
 		plansza[i] = new int[k];
 
-	for (int i = 0; i < w; i++)
-		for (int j = 0; j < k; j++)
-			plansza[i][j] = 0;
-
 	for (int i = 0; i < w; i++) {
-		cout << "O" << " ";
-		cout << endl;
 		for (int j = 0; j < k; j++) {
-			cout << "O" << " ";
+			plansza[i][j] = 0;
 		}
-
 	}
+	cout << endl;
+
+	cout << "\t\t\t\t" << "WARCABY: " << endl;
+	cout << endl;
+
+
+	for (int i = 0; i < 1; i++) {
+		cout << endl;
+		for (int j = 0; j < kolumny; j++) {
+			cout << " ";
+			cout << "\t" << j + 1 << "  ";
+			
+		}
+	}
+	
+	cout << endl;
+	for (int i = 0; i < 1; i++) {
+
+		for (int i = 0; i < wiersze; i++) {
+			cout << i + 1 << endl;
+			cout << endl;
+
+			for (int j = kolumny; j > 0; j--) {
+				cout << "\t" << "[]  ";
+			}
+
+		}
+	}
+};
+
+void wyczysc_plansze() {
+
 }
 
 
 int main()
 {
-
+	cout << "Witaj w grze WARCABY!" << endl;
 	rysuj_plansze();
-	
+
 }
 
